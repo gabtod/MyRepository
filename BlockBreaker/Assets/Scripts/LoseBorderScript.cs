3d65c7e9fd1fb1251d46f23a433a92ad9e9cd3b5
Gabriela Todorova <a100822@mcast.org.mt>
2017-11-13 10:00:53 +0100

    Handled trigger collision for lose border and loaded the Load Scene moved the paddle with the user's mouse lock the ball with the paddle for the beginning of the game

---
+++ BlockBreaker/Assets/Scripts/LoseBorderScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseBorderScript : MonoBehaviour {
+
+    //created an instance/copy og Level Mananger
+    LevelManager levelManager = new LevelManager();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger");
+        levelManager.LoadNewScene("Lose");
+    }
+
+
+}
